6493d20ebb476f86f5d6613c03ef2f59890fb23b
atlasmoth <ebuka422@gmail.com>
2024-06-27 07:56:13 +0100

    implemented permission based auth

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -55,6 +55,11 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
@@ -78,118 +83,115 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *application) activateUserHandler(w http.ResponseWriter, r *http.Request) {
-    var input struct {
-        TokenPlaintext string `json:"token"`
-    }
-    err := app.readJSON(w, r, &input)
-    if err != nil {
-        app.badRequestResponse(w, r, err)
-        return
-    }
+	var input struct {
+		TokenPlaintext string `json:"token"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
 
-    v := validator.New()
-    if data.ValidateTokenPlaintext(v, input.TokenPlaintext); !v.Valid() {
-        app.failedValidationResponse(w, r, v.Errors)
-        return
-    }
+	v := validator.New()
+	if data.ValidateTokenPlaintext(v, input.TokenPlaintext); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
 
-    user, err := app.models.Users.GetForToken(data.ScopeActivation, input.TokenPlaintext)
-    if err != nil {
-        switch {
-        case errors.Is(err, data.ErrRecordNotFound):
-            v.AddError("token", "invalid or expired activation token")
-            app.failedValidationResponse(w, r, v.Errors)
-        default:
-            app.serverErrorResponse(w, r, err)
-        }
-        return
-    }
+	user, err := app.models.Users.GetForToken(data.ScopeActivation, input.TokenPlaintext)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("token", "invalid or expired activation token")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
 
-    user.Activated = true
+	user.Activated = true
 
-    err = app.models.Users.Update(user)
-    if err != nil {
-        switch {
-        case errors.Is(err, data.ErrEditConflict):
-            app.editConflictResponse(w, r)
-        default:
-            app.serverErrorResponse(w, r, err)
-        }
-        return
-    }
+	err = app.models.Users.Update(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
 
-    err = app.models.Tokens.DeleteAllForUser(data.ScopeActivation, user.ID)
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-        return
-    }
+	err = app.models.Tokens.DeleteAllForUser(data.ScopeActivation, user.ID)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
-    err = app.writeJSON(w, http.StatusOK, envelope{"user": user}, nil)
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-    }
+	err = app.writeJSON(w, http.StatusOK, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
-
 
 func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
-    var input struct {
-        Email    string `json:"email"`
-        Password string `json:"password"`
-    }
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
-    err := app.readJSON(w, r, &input)
-    if err != nil {
-        app.badRequestResponse(w, r, err)
-        return
-    }
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
 
-    v := validator.New()
-    data.ValidateEmail(v, input.Email)
-    data.ValidatePasswordPlaintext(v, input.Password)
-    if !v.Valid() {
-        app.failedValidationResponse(w, r, v.Errors)
-        return
-    }
+	v := validator.New()
+	data.ValidateEmail(v, input.Email)
+	data.ValidatePasswordPlaintext(v, input.Password)
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
 
-    user, err := app.models.Users.GetByEmail(input.Email)
-    if err != nil {
-        switch {
-        case errors.Is(err, data.ErrRecordNotFound):
-            app.invalidCredentialsResponse(w, r)
-        default:
-            app.serverErrorResponse(w, r, err)
-        }
-        return
-    }
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.invalidCredentialsResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
 
-    match, err := user.Password.Matches(input.Password)
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-        return
-    }
-    if !match {
-        app.invalidCredentialsResponse(w, r)
-        return
-    }
+	match, err := user.Password.Matches(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	if !match {
+		app.invalidCredentialsResponse(w, r)
+		return
+	}
 
-    var claims jwt.Claims
-    claims.Subject = strconv.FormatInt(user.ID, 10)
-    claims.Issued = jwt.NewNumericTime(time.Now())
-    claims.NotBefore = jwt.NewNumericTime(time.Now())
-    claims.Expires = jwt.NewNumericTime(time.Now().Add(24 * time.Hour))
-    claims.Issuer = "atlasmoth"
-    claims.Audiences = []string{"atlasmoth"}
+	var claims jwt.Claims
+	claims.Subject = strconv.FormatInt(user.ID, 10)
+	claims.Issued = jwt.NewNumericTime(time.Now())
+	claims.NotBefore = jwt.NewNumericTime(time.Now())
+	claims.Expires = jwt.NewNumericTime(time.Now().Add(24 * time.Hour))
+	claims.Issuer = "atlasmoth"
+	claims.Audiences = []string{"atlasmoth"}
 
-    jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-        return
-    }
+	jwtBytes, err := claims.HMACSign(jwt.HS256, []byte(app.config.jwt.secret))
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
-    err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": string(jwtBytes)}, nil)
-    if err != nil {
-        app.serverErrorResponse(w, r, err)
-    }
+	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": string(jwtBytes)}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
-
-
